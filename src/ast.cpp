5c0b6a44291dcd60d5e020b7357feb7ae989a091
Rikka <2560359315@qq.com>
2025-06-23 13:19:02 +0800

    refactor: 从 `ast.h` 中拆分 AST 的初步实现到 `ast.cpp`

---
+++ src/ast.cpp
@@ -0,0 +1,140 @@
+#include "ast.h"
+
+// BaseAST implementations
+std::string BaseAST::toKoopa() const
+{
+    return "";
+}
+
+// NumberAST implementations
+NumberAST::NumberAST(int val)
+    : value(val)
+{
+}
+
+void NumberAST::Dump() const
+{
+    std::cout << "NumberAST { " << value << " }";
+}
+
+std::string NumberAST::toKoopa() const
+{
+    return std::to_string(value);
+}
+
+// FuncTypeAST implementations
+FuncTypeAST::FuncTypeAST(const std::string& name)
+    : type_name(name)
+{
+}
+
+void FuncTypeAST::Dump() const
+{
+    std::cout << "FuncTypeAST { " << type_name << " }";
+}
+
+std::string FuncTypeAST::toKoopa() const
+{
+    return koopa_type_map.at(type_name);
+}
+
+// StmtAST implementations
+StmtAST::StmtAST(std::unique_ptr<NumberAST> num)
+    : number(std::move(num))
+{
+}
+
+void StmtAST::Dump() const
+{
+    std::cout << "StmtAST { return ";
+    if (number) {
+        number->Dump();
+    } else {
+        std::cout << "null";
+    }
+    std::cout << "; }";
+}
+
+std::string StmtAST::toKoopa() const
+{
+    if (number) {
+        return stringFormat("ret %s\n", number->toKoopa());
+    }
+    return "ret void\n"; // 如果没有数字，返回 void
+}
+
+// BlockAST implementations
+BlockAST::BlockAST(std::unique_ptr<StmtAST> s)
+    : stmt(std::move(s))
+{
+}
+
+void BlockAST::Dump() const
+{
+    std::cout << "BlockAST { ";
+    if (stmt) {
+        stmt->Dump();
+    } else {
+        std::cout << "empty";
+    }
+    std::cout << " }";
+}
+
+std::string BlockAST::toKoopa() const
+{
+    if (stmt) {
+        return stringFormat("  %s", stmt->toKoopa());
+    }
+    return "";
+}
+
+// FuncDefAST implementations
+FuncDefAST::FuncDefAST(std::unique_ptr<FuncTypeAST> type, const std::string& id, std::unique_ptr<BlockAST> blk)
+    : func_type(std::move(type))
+    , ident(id)
+    , block(std::move(blk))
+{
+}
+
+void FuncDefAST::Dump() const
+{
+    std::cout << "FuncDefAST { ";
+    func_type->Dump();
+    std::cout << ", " << ident << ", ";
+    block->Dump();
+    std::cout << " }";
+}
+
+std::string FuncDefAST::toKoopa() const
+{
+    auto is_entry_fun = (ident == "main" && func_type->type_name == "int");
+    // std::cout << "[DEBUG] func_type: " << func_type->type_name << ", ident: " << ident << " , isEntry" << isEntryFun << std::endl;
+    return stringFormat("fun @%s(%s): %s {\n%s%s}",
+        ident, // 标识符
+        "", // 参数列表，暂时留空
+        func_type->toKoopa(), // 返回类型
+        is_entry_fun ? "\%entry:\n" : "", // 如果是入口函数，添加 entry
+        block->toKoopa() // 函数体
+    );
+}
+
+// CompUnitAST implementations
+CompUnitAST::CompUnitAST(std::unique_ptr<FuncDefAST> func)
+    : func_def(std::move(func))
+{
+}
+
+void CompUnitAST::Dump() const
+{
+    std::cout << "CompUnitAST { ";
+    func_def->Dump();
+    std::cout << " }";
+}
+
+std::string CompUnitAST::toKoopa() const
+{
+    if (func_def) {
+        return func_def->toKoopa();
+    }
+    return "";
+}
--- src/ast.h
+++ src/ast.h
@@ -22,9 +22,7 @@ public:
     virtual ~BaseAST() = default;
 
     virtual void Dump() const = 0;
-    virtual std::string toKoopa() const {
-        return "";
-    }
+    virtual std::string toKoopa() const;
 };
 
 // Number
@@ -32,17 +30,10 @@ class NumberAST : public BaseAST {
 public:
     int value;
 
-    NumberAST(int val) : value(val) {}
+    NumberAST(int val);
 
-    void Dump() const override
-    {
-        std::cout << "NumberAST { " << value << " }";
-    }
-
-    std::string toKoopa() const override
-    {
-        return std::to_string(value);
-    }
+    void Dump() const override;
+    std::string toKoopa() const override;
 };
 
 // FuncType
@@ -51,22 +42,15 @@ public:
     std::string type_name; // "int"
     std::string ident; // "main"
 
-    FuncTypeAST(const std::string &name) : type_name(name) {}
+    FuncTypeAST(const std::string& name);
 
-    void Dump() const override
-    {
-        std::cout << "FuncTypeAST { " << type_name << " }";
-    }
-
-    std::string toKoopa() const override
-    {
-        return koopa_type_map.at(type_name);
-    }
+    void Dump() const override;
+    std::string toKoopa() const override;
 
 private:
     std::map<std::string, std::string> koopa_type_map = {
-        {"int", "i32"},
-        {"void", "void"},
+        { "int", "i32" },
+        { "void", "void" },
     };
 };
 
@@ -76,26 +60,10 @@ public:
     // Stmt -> "return" Number ";"
     std::unique_ptr<NumberAST> number;
 
-    StmtAST(std::unique_ptr<NumberAST> num) : number(std::move(num)) {}
+    StmtAST(std::unique_ptr<NumberAST> num);
 
-    void Dump() const override
-    {
-        std::cout << "StmtAST { return ";
-        if (number) {
-            number->Dump();
-        } else {
-            std::cout << "null";
-        }
-        std::cout << "; }";
-    }
-
-    std::string toKoopa() const override
-    {
-        if (number) {
-            return stringFormat("ret %s\n", number->toKoopa());
-        }
-        return "ret void\n"; // 如果没有数字，返回 void
-    }
+    void Dump() const override;
+    std::string toKoopa() const override;
 };
 
 // Block
@@ -103,26 +71,10 @@ class BlockAST : public BaseAST {
 public:
     std::unique_ptr<StmtAST> stmt;
 
-    BlockAST(std::unique_ptr<StmtAST> s) : stmt(std::move(s)) {}
+    BlockAST(std::unique_ptr<StmtAST> s);
 
-    void Dump() const override
-    {
-        std::cout << "BlockAST { ";
-        if (stmt) {
-            stmt->Dump();
-        } else {
-            std::cout << "empty";
-        }
-        std::cout << " }";
-    }
-
-    std::string toKoopa() const override
-    {
-        if (stmt) {
-            return stringFormat("  %s", stmt->toKoopa());
-        }
-        return "";
-    }
+    void Dump() const override;
+    std::string toKoopa() const override;
 };
 
 // FuncDef 也是 BaseAST
@@ -132,30 +84,10 @@ public:
     std::string ident;
     std::unique_ptr<BlockAST> block;
 
-    FuncDefAST(std::unique_ptr<FuncTypeAST> type, const std::string &id, std::unique_ptr<BlockAST> blk)
-        : func_type(std::move(type)), ident(id), block(std::move(blk)) {}
+    FuncDefAST(std::unique_ptr<FuncTypeAST> type, const std::string& id, std::unique_ptr<BlockAST> blk);
 
-    void Dump() const override
-    {
-        std::cout << "FuncDefAST { ";
-        func_type->Dump();
-        std::cout << ", " << ident << ", ";
-        block->Dump();
-        std::cout << " }";
-    }
-
-    std::string toKoopa() const override
-    {
-        auto is_entry_fun = (ident == "main" && func_type->type_name == "int");
-        // std::cout << "[DEBUG] func_type: " << func_type->type_name << ", ident: " << ident << " , isEntry" << isEntryFun << std::endl;
-        return stringFormat("fun @%s(%s): %s {\n%s%s}", 
-            ident,  // 标识符
-            "",  // 参数列表，暂时留空
-            func_type->toKoopa(), // 返回类型
-            is_entry_fun ? "\%entry:\n" : "", // 如果是入口函数，添加 entry
-            block->toKoopa() // 函数体
-        );
-    }
+    void Dump() const override;
+    std::string toKoopa() const override;
 };
 
 // CompUnit 是 BaseAST
@@ -164,20 +96,8 @@ public:
     // 用智能指针管理对象
     std::unique_ptr<FuncDefAST> func_def;
 
-    CompUnitAST(std::unique_ptr<FuncDefAST> func) : func_def(std::move(func)) {}
+    CompUnitAST(std::unique_ptr<FuncDefAST> func);
 
-    void Dump() const override
-    {
-        std::cout << "CompUnitAST { ";
-        func_def->Dump();
-        std::cout << " }";
-    }
-
-    std::string toKoopa() const override
-    {
-        if (func_def) {
-            return func_def->toKoopa();
-        }
-        return "";
-    }
+    void Dump() const override;
+    std::string toKoopa() const override;
 };
